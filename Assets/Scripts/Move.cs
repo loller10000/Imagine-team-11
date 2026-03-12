6358f835fec45112b9f66a959a01548f97871446
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-03-12 16:18:45 +0100

    Added destroy section, building prefab
    Played with values left comments in code and discord.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,16 +3,24 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     [Range (1f, 100f)] [SerializeField] private float moveSpeedZ;
+    
+    /// <summary>
+    /// Player feedback tot nu toe:
+    /// Yntze: 10 lijkt me wel een goeie default
+    /// Tom M.: Vindt het te sloom, lijkt eerder walking raad aan rond de ~20 instead.
+    /// Ik ben (Jahva) ben het eens met deze statement is meer sprinting gevoel.
+    /// </summary>
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(0, 0, -moveSpeedZ) * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Destroy"))
+        {
+            Destroy(gameObject);
+        }    
     }
 }
--- Assets/Scripts/SectionTrigger.cs
+++ Assets/Scripts/SectionTrigger.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class SectionTrigger : MonoBehaviour
 {
     public GameObject roadSection;
+    [SerializeField] private float transformZ = 8; // NEW DEFAULT
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Trigger"))
         {
-            Instantiate(roadSection, new Vector3(0, 0, 5), Quaternion.identity);
+            Instantiate(roadSection, new Vector3(0, 0, transformZ), Quaternion.identity);
         }
     }
 }
