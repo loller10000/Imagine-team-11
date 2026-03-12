b1bb18d81c6f82eaac49d64fc924b517f4133ec6
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-03-12 16:26:06 +0100

    More feedback

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,9 +6,12 @@ public class Move : MonoBehaviour
     
     /// <summary>
     /// Player feedback tot nu toe:
-    /// Yntze: 10 lijkt me wel een goeie default
+    /// Yntze: 10 lijkt me wel een goeie default, als of je aant lopen/joggen bent.
     /// Tom M.: Vindt het te sloom, lijkt eerder walking raad aan rond de ~20 instead.
     /// Ik ben (Jahva) ben het eens met deze statement is meer sprinting gevoel.
+    /// Andy: 40, nu rent ie zo snel als een auto lijkt het wel.
+    /// Christian Roth: Increase speed so it becomes gradually faster
+    /// And vary between platform size sometimes bigger, sometimes smaller.
     /// </summary>
 
     void Update()
