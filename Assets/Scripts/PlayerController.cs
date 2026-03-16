f091ac03dd7ee033ca7ed27ce4e2e6a218475a76
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-03-16 22:32:13 +0100

    Seperate logic between update and fixed update.
    Also added a physics material to simulate movement with 0 friction, and set the sphere's rigidbody to interpolate for smoother feel. Added trail to visualize the movement better.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,46 +8,32 @@ public class PlayerController : MonoBehaviour
     //[SerializeField] private Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
     
     private Vector3 velocity;
+    private Vector3 desiredVelocity;
+    private Rigidbody body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
-        //playerInput.Normalize();
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        body.linearVelocity = velocity;
         
-        var desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
         var maxSpeedChange = maxAcceleration * Time.deltaTime;
         
         velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
         velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
-        
-        var displacement = velocity * Time.deltaTime;
-        var newPosition = transform.localPosition + displacement;
-        
-        // if (newPosition.x < allowedArea.xMin) 
-        // {
-        //     newPosition.x = allowedArea.xMin;
-        //     velocity.x = -velocity.x * bounciness;
-        // }
-        // else if (newPosition.x > allowedArea.xMax) 
-        // {
-        //     newPosition.x = allowedArea.xMax;
-        //     velocity.x = -velocity.x * bounciness;
-        // }
-        // if (newPosition.z < allowedArea.yMin) 
-        // {
-        //     newPosition.z = allowedArea.yMin;
-        //     velocity.z = -velocity.z * bounciness;
-        // }
-        // else if (newPosition.z > allowedArea.yMax) 
-        // {
-        //     newPosition.z = allowedArea.yMax;
-        //     velocity.z = -velocity.z * bounciness;
-        // }
-        
-        transform.localPosition = newPosition;
     }
 
     private void Move()
@@ -58,7 +44,7 @@ public class PlayerController : MonoBehaviour
         // playerInput.Normalize(); // More limited movement. 
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
         
-        var desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
         var maxSpeedChange = maxAcceleration * Time.deltaTime;
         
         velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
