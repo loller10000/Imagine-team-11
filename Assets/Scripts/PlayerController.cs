674474a9ef3c64862a9bf16d475796cb7614bfce
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-03-17 19:09:31 +0100

    fixed movement, forgot to apply velocity..
    body.linearVelocity = velocity;

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,27 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Range(0f, 100f)] private float maxAirAcceleration = 1f;
     [SerializeField, Range(0f, 10f)] private float jumpHeight = 2f;
     [SerializeField, Range(0f, 5f)] private int maxAirJumps = 0;
+    [SerializeField, Range(0f, 90f)] private float maxGroundAngle = 25f;
     
     private Vector3 velocity;
     private Vector3 desiredVelocity;
+    private Vector3 contactNormal;
     private Rigidbody body;
     
     [SerializeField] private bool desiredJump;
     [SerializeField] private bool onGround;
     [SerializeField] private int jumpPhase;
+    private float minGroundDotProduct;
+
+    private void OnValidate()
+    {
+        minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+    }
     
     private void Awake()
     {
         body = GetComponent<Rigidbody>();
+        OnValidate();
     }
 
     private void Update()
@@ -35,6 +44,7 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         UpdateState();
+        AdjustVelocity();
         
         if (desiredJump)
         {
@@ -42,13 +52,8 @@ public class PlayerController : MonoBehaviour
             Jump();
         }
         
-        var acceleration = onGround ? maxAcceleration : maxAirAcceleration;
-        var maxSpeedChange = acceleration * Time.deltaTime;
-        
-        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
-        
-        onGround = false; // Keep this check at END of FixedUpdate.
+        body.linearVelocity = velocity;
+        onGround = false;
     }
     
     private void UpdateState()
@@ -59,6 +64,11 @@ public class PlayerController : MonoBehaviour
         {
             jumpPhase = 0;
         }
+        
+        else
+        {
+            contactNormal = Vector3.up;
+        }
     }
     
     private void Jump()
@@ -66,14 +76,17 @@ public class PlayerController : MonoBehaviour
         if (onGround || jumpPhase < maxAirJumps)
         {
             jumpPhase += 1;
-            var jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
             
-            if (velocity.y > 0f)
+            var jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+            var alignedSpeed = Vector3.Dot(velocity, contactNormal);
+            
+            if (alignedSpeed > 0f)
             {
-                jumpSpeed = Mathf.Max(jumpSpeed = velocity.y, 0f);
+                jumpSpeed = Mathf.Max(jumpSpeed = alignedSpeed, 0f);
             }
             
-            velocity.y += jumpSpeed;
+            //velocity.y += jumpSpeed;
+            velocity += contactNormal * jumpSpeed;
         }
     }
     
@@ -92,8 +105,35 @@ public class PlayerController : MonoBehaviour
         for (int i = 0; i < collision.contactCount; i++)
         {
             Vector3 normal = collision.GetContact(i).normal;
-            onGround |= normal.y >= 0.9f;
+            
+            if (normal.y >= minGroundDotProduct)
+            {
+                onGround = true;
+                contactNormal = normal;
+            }
         }
+    }
+
+    private Vector3 ProjectOnContactPlane(Vector3 vector)
+    {
+        return vector - contactNormal * Vector3.Dot(vector, contactNormal);
+    }
+
+    private void AdjustVelocity()
+    {
+        var xAxis = ProjectOnContactPlane(Vector3.right).normalized;
+        var zAxis = ProjectOnContactPlane(Vector3.forward).normalized;
+        
+        var currentX = Vector3.Dot(velocity, xAxis);
+        var currentZ = Vector3.Dot(velocity, zAxis);
+
+        var acceleration = onGround ? maxAcceleration : maxAirAcceleration;
+        var maxSpeedChange = acceleration * Time.deltaTime;
+        
+        var newX = Mathf.MoveTowards(currentX, desiredVelocity.x, maxSpeedChange);
+        var newZ = Mathf.MoveTowards(currentZ, desiredVelocity.z, maxSpeedChange);
+        
+        velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
     }
 
     private void Move()
