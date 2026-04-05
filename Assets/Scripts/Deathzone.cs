745a7054619825f54bb6f6096738568607d11584
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-04-05 21:09:40 +0200

    Sort of death mechanic added.

---
+++ Assets/Scripts/Deathzone.cs
@@ -0,0 +1,20 @@
+using Unity.VectorGraphics;
+using UnityEngine;
+
+public class Deathzone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // TODO: Make a playerStats class or smth to contain lives.
+            Invoke(nameof(Restart), 2);
+        }
+    }
+
+    // Reloads the scene after 2 seconds
+    private void Restart()
+    {
+        SceneLoader.LoadScene("TestScene");
+    }
+}
